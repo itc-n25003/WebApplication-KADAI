8843d2a570c45c55cabdbb271ec6fce660d6804a
itc-n25003 <n25003@std.it-college.ac.jp>
2026-02-10 12:33:19 +0900

    0210進捗

--- app/infopage/page.tsx
+++ app/infopage/page.tsx
@@ -1,18 +1,36 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 import ButtonLink from "@/app/ButtonLink";
 
 export default function Page() {
   return (
     <>
-      <iframe
-        className={styles.pullsite}
-        src="https://senka.su/world?num=1"
-        style={{
-          zIndex: 1000,
-        }}
-      />
-      <div>
-        <ButtonLink href="">横須賀鎮守府</ButtonLink>
+      <div className={styles.grid}>
+        <Image
+          className={styles.bg}
+          src="/drop_bg_blue.png"
+          alt=""
+          width={800}
+          height={480}
+        />
+        <iframe
+          className={styles.pullsite}
+          src="https://senka.su/world?num=1"
+          style={{
+            zIndex: 1000,
+          }}
+        />
+      </div>
+      <div className={styles.otherpage}>
+        <a className={styles.link} href="">
+          ⇠柱島泊地
+        </a>
+        <a className={styles.link} href="/..">
+          TOP PAGE
+        </a>
+        <a className={styles.link} href="">
+          呉鎮守府⇢
+        </a>
       </div>
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 import ButtonLink from "./ButtonLink";
 
 export default function Page() {
   return (
     <>
+      <Image
+        className={styles.topimage}
+        src="/topimage.png"
+        alt="後で差し替え"
+        width={1200}
+        height={450}
+      />
       <div className={styles.grid}>
-        <ButtonLink href="info/page.tsx">横須賀鎮守府</ButtonLink>
+        <Image
+          className={styles.bg}
+          src="/drop_bg_blue.png"
+          alt=""
+          width={800}
+          height={480}
+        />
+        <ButtonLink href="/infopage/">横須賀鎮守府</ButtonLink>
         <ButtonLink href="info/page.tsx">呉鎮守府</ButtonLink>
         <ButtonLink href="info/page.tsx">佐世保鎮守府</ButtonLink>
         <ButtonLink href="info/page.tsx">舞鶴鎮守府</ButtonLink>
@@ -19,6 +34,12 @@ export default function Page() {
         <ButtonLink href="info/page.tsx">タウイタウイ泊地</ButtonLink>
         <ButtonLink href="info/page.tsx">パラオ泊地</ButtonLink>
         <ButtonLink href="info/page.tsx">ブルネイ泊地</ButtonLink>
+        <ButtonLink href="info/page.tsx">単冠湾泊地</ButtonLink>
+        <ButtonLink href="info/page.tsx">幌筵泊地</ButtonLink>
+        <ButtonLink href="info/page.tsx">鹿屋泊地</ButtonLink>
+        <ButtonLink href="info/page.tsx">岩川基地</ButtonLink>
+        <ButtonLink href="info/page.tsx">佐伯湾泊地</ButtonLink>
+        <ButtonLink href="info/page.tsx">柱島泊地</ButtonLink>
       </div>
     </>
   );
